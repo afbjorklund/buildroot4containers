61ffdcc34ff1a72e4403df81f0d9cf80b7d2df23
Anders F Björklund <anders.f.bjorklund@gmail.com>
2021-10-03 13:11:18 +0200

    Remove old docker config for rootfs
    (cherry picked from commit 163ca980472ff3db8c1738f057f2f29490f73e92)

--- external/post_build.sh
+++ external/post_build.sh
@@ -11,8 +11,3 @@ if [ -e ${TARGET_DIR}/etc/inittab ]; then
 	sed -i '/GENERIC_SERIAL/a\
 tty1::respawn:/sbin/getty -L  tty1 0 vt100 # QEMU graphical window' ${TARGET_DIR}/etc/inittab
 fi
-
-# can't use pivot_root when running on rootfs, only when root is on e.g. tmpfs
-
-mkdir -p $TARGET_DIR/etc/default
-echo "export DOCKER_RAMDISK=true" > $TARGET_DIR/etc/default/dockerd
