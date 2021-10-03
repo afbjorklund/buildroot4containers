4ce1ab72deece96c4ff315bc66a449c05d1a699d
Anders F Björklund <anders.f.bjorklund@gmail.com>
2021-10-03 19:01:31 +0200

    Remove old qemu script for creating tty1

--- external/post_build.sh
+++ external/post_build.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-set -u
 set -e
 
 BOARD_DIR=$(dirname "$0")
@@ -10,9 +9,3 @@ cp -f "$BOARD_DIR/grub-bios.cfg" "$TARGET_DIR/boot/grub/grub.cfg"
 # Copy grub 1st stage to binaries, required for genimage
 cp -f "$HOST_DIR/lib/grub/i386-pc/boot.img" "$BINARIES_DIR"
 
-# Add a console on tty1
-if [ -e ${TARGET_DIR}/etc/inittab ]; then
-    grep -qE '^tty1::' ${TARGET_DIR}/etc/inittab || \
-	sed -i '/GENERIC_SERIAL/a\
-tty1::respawn:/sbin/getty -L  tty1 0 vt100 # QEMU graphical window' ${TARGET_DIR}/etc/inittab
-fi
